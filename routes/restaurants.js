d1a02d439a96868a8e3719b12f97242ba09672d1
Daniel <danieldelacruz01@gmail.com>
2016-03-16 17:59:12 +1300

    add environment variables

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var request = require('superagent')
+var dotenv = require('dotenv')
+
+//load environment variables
+dotenv.load()
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('restaurants');
 });
 
 module.exports = router;
