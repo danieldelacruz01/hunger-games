86db71d0520ffed28b3513f0efcfd90d3a7fba0c
Daniel <danieldelacruz01@gmail.com>
2016-03-23 11:01:41 +1300

    GET/restaurants makes a request to zomato api using test query

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -3,12 +3,41 @@ var router = express.Router();
 var request = require('superagent')
 var dotenv = require('dotenv')
 
+var testLat = -41.2969092
+var testLon = 174.7720306
+
 //load environment variables
 dotenv.load()
 
-/* GET users listing. */
+restaurants = []
+
+function searchZomato(obj, callback){
+	request
+		.get('https://developers.zomato.com/api/v2.1/search')
+		.set('user-key', process.env.ZOMATO)
+		.query(obj)
+		.end(function(err,res){
+			// console.log(res.body)
+			callback(res.body)
+		})	
+}
+
+/* GET restaurants listing. */
 router.get('/', function(req, res, next) {
-  res.send('restaurants');
+
+	var queryObj = {
+		count: 10,
+		lat: -41.2969092,
+		lon: 174.7720306,
+		radius: 500,
+		sort: 'real_distance',
+		order: 'asc'
+	}
+
+	searchZomato(queryObj, function(result) {
+		res.send(result)
+	})
 });
+
 
 module.exports = router;
