fad0abb95787e4f3fb8a4f77e113fa0ea6331621
Joshua Vial <joshua@enspiral.com>
2016-03-25 14:51:20 +1300

    initial refactoring

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 var $ = require('jquery')
-var request = require('superagent')
 var h = require('hyperscript')
+var client = require('./apiClient')
 
 $(document).ready(function() {
 
@@ -27,62 +27,6 @@ $(document).ready(function() {
 	}
 	autocomplete = new google.maps.places.Autocomplete(input, options);
 	
-	$('#get-coords').click(function(){
-		request
-			.get('http://maps.googleapis.com/maps/api/geocode/json')
-			.query({
-				address: $('#searchTextField').val(),
-				key: process.env.GMAPS
-			})
-			.end(function(err,res){
-				var coords = res.body.results[0].geometry.location
-				$('#lat').val(coords.lat)
-				$('#lon').val(coords.lng)
-			})
-	})
-
-	var query = {
-		count: 3,
-		radius: 500,
-		sort: 'rating',
-		order: 'desc'
-	}
-
-	$('#submit-query').click(function(){
-		query['lat'] = $('input#lat').val()
-		query['lon'] = $('input#lon').val()
-		
-		request
-			.get('../restaurants')
-			.query(query)
-			.end(function(err,res){
-				createDiv()
-				appendResults(res.body)
-			})
-	})
-
-	function createDiv(){
-		$('body').append(
-			h('div#results',
-				h('h2', 'You should try:')
-			)
-		)
-	}
-	
-
-	function appendResults(resObj){
-		for (var i = 0; i < resObj.restaurants.length; i++){
-			$('div#results').append(		
-				h('h3', resObj.restaurants[i].name),
-				h('p', resObj.restaurants[i].location.address),
-				h('ul',
-					h('li', 'Cuisine: '+resObj.restaurants[i].cuisines),
-					h('li', 'Rating: '+resObj.restaurants[i].rating),
-					h('li',
-						h('a','View Menu',{'href':resObj.restaurants[i].menu})
-					)
-				)
-			)
-		}
-	}
+	$('#get-coords').click(client.getGoogleMapData)
+	$('#submit-query').click(client.getRestaurantData)
 });
