5f355042bb7712c8b05eea6d75ef31574ca00f0b
Daniel <danieldelacruz01@gmail.com>
2016-05-12 21:32:52 +1200

    add favicon

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var app = express();
 var path = require('path');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
+var favicon = require('serve-favicon');
 
 var routes = require('./routes/index');
 var restaurants = require('./routes/restaurants');
@@ -12,6 +13,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(favicon(path.join(__dirname,'public','images','favicon.ico')));
 
 app.use('/', routes);
 app.use('/restaurants', restaurants);
